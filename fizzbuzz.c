4238314686b07e2804cf2a7bdc5ab62fa274f857
Kazuki Minaguchi <j2034@matsue-ct.ac.jp>
2023-12-07 14:04:16 +0900

    プログラム修正

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-
-void hello()
-{
-printf("HELLO WORLD!!!!!!!!!\n");
-printf("GOOD MORNING!!!!!!!\n")
-}
-
 int main()
 {
-hello();
+for(int i=0;i < 100;i++){
+if(i % 3 = 0)
+printf("Fizz ");
+if(i % 5 = 0)
+printf("Buzz ");
+if(i % 7 = 0)
+printf("Git ");
+}
 return 0;
 }
